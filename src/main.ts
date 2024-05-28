10fb0e309c153e9c7d70f3c8644167bca7c4d902
Evrige <artur.dudnik@inbox.ru>
2024-05-28 20:10:35 +0300

    ADD: front vercel path

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { ValidationPipe } from "./pipes/validation.pipes";
 import * as cookieParser from "cookie-parser";
 import * as express from "express";
 import * as path from "path";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import { join } from "path";
 import * as fs from "node:fs";
 
 dotenv.config();
@@ -16,16 +18,15 @@ async function bootstrap() {
 
   const config = new DocumentBuilder()
     .setTitle('Backend MTG Application')
-    .setDescription('Документация для бэкенда')
+    .setDescription('Documentation for the backend')
     .setVersion('1.0.0')
     .addTag('by Evrige')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/api/docs', app, document);
   app.setGlobalPrefix('api');
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe())
   app.use(cookieParser());
-
   const uploadsDir = path.join(__dirname, '..', 'uploads');
   console.log('Сервируем статические файлы из:', uploadsDir);
 
@@ -35,15 +36,13 @@ async function bootstrap() {
   }
 
   app.use('/uploads', express.static(uploadsDir));
-
   app.enableCors({
     origin: ['http://localhost:3000', 'https://tournament-frontend-sable.vercel.app'],
     credentials: true
   });
 
   await app.listen(PORT, () => {
-    console.log('Слушаем порт ' + PORT);
+    console.log('listening on port ' + PORT);
   });
 }
-
 bootstrap();
