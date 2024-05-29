9dc5a3046db051f2299483370337c40023c56f2f
Evrige <artur.dudnik@inbox.ru>
2024-05-29 23:24:17 +0300

    ADD: front vercel path

--- src/models/auth/auth.controller.ts
+++ src/models/auth/auth.controller.ts
@@ -111,12 +111,13 @@ export class AuthController {
 			httpOnly: true,
 			secure: true,
 			expires: new Date(Date.now() + 1 * 30 * 60 * 1000),
+			sameSite: false
 		});
 		res.cookie("refreshToken", refreshToken, {
 			httpOnly: true,
 			secure: true,
-			expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000)
-			,
+			expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
+			sameSite: false
 		});
 	}
 }
